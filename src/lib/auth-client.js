ba0a00a8d3f98d32037f8accc8aedcd98eb438f1
saharatajim <sahara.tajim@gmail.com>
2026-05-01 07:29:24 +0600

    repo change

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,6 +2,7 @@ import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
  baseURL: process.env.BETTER_AUTH_URL,
 })
+
 export const { signIn, signUp, useSession } = createAuthClient()
 
 
